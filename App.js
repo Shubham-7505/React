69d38bba81f86cf9a55166de13fd957a5780bd6d
Shubham Kumar <shubham07pal@gmail.com>
2024-12-15 12:57:05 +0530

    Learning_React

---
+++ App.js
@@ -0,0 +1,23 @@
+// const heading = React.createElement(
+//     "h1",
+//     {id:"heading"},
+//     "Hello, My name is Shubham Kumar."
+// );
+
+const parent = React.createElement("div",
+  {id:"parent"}, 
+  [React.createElement("div", {id:"child1"},
+    [React.createElement("h1", {}, "I am h1 tag form child 1"),
+     React.createElement("h2", {}, "I am h2 tag form child 1")
+    ]
+  ),
+  React.createElement("div", {id:"child2"},
+    [React.createElement("h1", {}, "I am h1 tag form child 2"),
+     React.createElement("h2", {}, "I am h2 tag form child 2")
+    ]
+  )
+]
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
